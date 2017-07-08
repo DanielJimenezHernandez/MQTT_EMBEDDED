637973a6e6f4313965194a7c6aaf5e622ca6c85e
Daniel Jimenez <danieljimher@hotmail.es>
2017-07-08 02:23:06 -0500

    Bridge finished (working for publishing messages) and a client udpclient.c simulating a udp mqtt client

--- udp_tcp_mqtt_bridge.c
+++ udp_tcp_mqtt_bridge.c
@@ -21,15 +21,31 @@
 #define MQTT_PORT 1883
 #define UDP_PORT 8888
 
-sem_t receivedUdpMutex,receivedTcpMutex,firstConnectMutex;
-char sharedUdpBuffer[BUFSIZE];
-char sharedTcpBuffer[BUFSIZE];
+sem_t receivedUdpMutex,receivedTcpMutex,tcpMsgWait;
+unsigned char sharedUdpBuffer[BUFSIZE];
+unsigned char sharedTcpBuffer[BUFSIZE];
 int udp_mqtt_len = 0;
+int first_run_flag = 1;
+int reconnectFlag = 1;
+unsigned char disconnect_package[2] = {0xe0,0x00};
+
+char * strBrokerAddr,strBrokerPort,strUdpPort;
+int brokerPort;
+int udpPort;
 
 /* Catch Signal Handler functio */
 void signal_callback_handler(int signum){
 
         printf("Caught signal SIGPIPE %d\n",signum);
+}
+
+void printMessage(unsigned char *buf,int len,const char* description){
+  int i;
+  printf("%s -> [0x%02x]\n",description,buf[0]);
+  for (i = 0; i < len; i++){
+    printf("[0x%02x]",buf[i]);
+  }
+  printf("\n");
 }
 
 /*
@@ -48,7 +64,7 @@ void die(char *s)
 void *UDPServer(void *port){
   struct sockaddr_in si_me, si_other;
 
-  int s, i, slen = sizeof(si_other) , recv_len,true;
+  int s, slen = sizeof(si_other) , recv_len,true;
 
   //create a UDP socket
   if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
@@ -62,7 +78,7 @@ void *UDPServer(void *port){
   memset((char *) &si_me, 0, sizeof(si_me));
 
   si_me.sin_family = AF_INET;
-  si_me.sin_port = htons(UDP_PORT);
+  si_me.sin_port = htons(udpPort);
   si_me.sin_addr.s_addr = htonl(INADDR_ANY);
 
   //bind socket to port
@@ -74,41 +90,47 @@ void *UDPServer(void *port){
   //keep listening for data
   while(1)
   {
-      printf("Waiting for data...");
+      //printf("Waiting for data...");
       fflush(stdout);
 
       //try to receive some data, this is a blocking call
-      if ((recv_len = recvfrom(s, sharedUdpBuffer, BUFSIZE, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+      if ((recv_len = recvfrom(s, sharedUdpBuffer, BUFSIZE, 0, (struct sockaddr *) &si_other,(socklen_t *) &slen)) == -1)
       {
           die("recvfrom()");
       }
-
+      sem_post (&receivedUdpMutex);
       //print details of the client/peer and the data received
       udp_mqtt_len = (int)sharedUdpBuffer[1] + 2;
-      sem_post (&receivedUdpMutex);
-      sem_post (&firstConnectMutex);
-
+      printMessage(sharedUdpBuffer,udp_mqtt_len,"UDP Recieved");
+      if (first_run_flag){
+        first_run_flag = 0;
+      }
 
       //now reply the client with the  data from the tcp server
-      sem_wait (&receivedTcpMutex);
-      if (sendto(s, sharedTcpBuffer, strlen(sharedTcpBuffer), 0, (struct sockaddr*) &si_other, slen) == -1)
-      {
-          die("sendto()");
-      }
+      //If The command arrived from UDP is for a connect expect response ACK
+      if ((sharedUdpBuffer[0] >> 4) == CONNECT){
+        sem_wait (&receivedTcpMutex);
+        printMessage(sharedTcpBuffer,(int)sharedTcpBuffer[1],"UDP Send");
+        if (sendto(s, sharedTcpBuffer, (int)sharedTcpBuffer[1], 0, (struct sockaddr*) &si_other, slen) == -1)
+        {
+            die("sendto()");
+        }
+        /* wait for tcp message to be sent Syn Issue */
+
+    }
+    sem_wait(&tcpMsgWait);
   }
 
   close(s);
 }
 
 void *TCPClient(void *args){
-  int sock,true,reconnectFlag;
+  int sock,true;
   struct sockaddr_in server;
-  char message[1000] , server_reply[2000];
 
 
   //Create socket
-  sem_wait (&firstConnectMutex);
-  RECONNECT:
+  while(first_run_flag);
   sock = socket(AF_INET , SOCK_STREAM , 0);
   if (sock == -1)
   {
@@ -119,45 +141,42 @@ void *TCPClient(void *args){
   setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&true,sizeof(int));
   puts("Socket created");
 
-  server.sin_addr.s_addr = inet_addr("192.168.1.5");
+  server.sin_addr.s_addr = inet_addr(strBrokerAddr);
   server.sin_family = AF_INET;
-  server.sin_port = htons(MQTT_PORT);
+  server.sin_port = htons(brokerPort);
 
   //Connect to remote server
 
-  if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-  {
-      perror("connect failed. Error");
-      return 1;
-  }
-
-  puts("Connected\n");
 
   //keep communicating with server
   while(1)
   {
-      if(!reconnectFlag)
-        sem_wait (&receivedUdpMutex);
-      else
+      if(reconnectFlag){
+        if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+        {
+            perror("connect failed. Error");
+        }
         reconnectFlag = 0;
-
+      }
       //Send some data
+      sem_wait (&receivedUdpMutex);
+      printMessage(sharedUdpBuffer,udp_mqtt_len,"TCP Send");
       if( send(sock , sharedUdpBuffer , udp_mqtt_len , 0) < 0)
       {
           puts("Send failed");
-          reconnectFlag = 1;
-          goto RECONNECT;
       }
-      memset(sharedUdpBuffer,0,BUFSIZE);
+      sem_post(&tcpMsgWait);
+      /*stop the udp thread until the mesage is sent*/
 
       //Receive a reply from the server
-      if( recv(sock , sharedTcpBuffer , BUFSIZE , 0) < 0)
-      {
-          puts("recv failed");
+      if ((sharedUdpBuffer[0] >> 4) == CONNECT){
+        if( recv(sock , sharedTcpBuffer , BUFSIZE , 0) < 0)
+        {
+            puts("TCP: recv failed");
+        }
+        printMessage(sharedTcpBuffer,sharedTcpBuffer[1],"TCP Recieved");
+        sem_post (&receivedTcpMutex);
       }
-      sem_post (&receivedTcpMutex);
-      puts("TCP Server reply :");
-      puts(sharedTcpBuffer);
   }
 
   close(sock);
@@ -166,16 +185,20 @@ void *TCPClient(void *args){
 int main(int argc, char **argv) {
 
    /* check command line arguments */
-   if (argc != 3) {
-      fprintf(stderr,"usage: %s <MQTT Broker Addr> <udp bridge server port>\n", argv[0]);
+   if (argc != 4) {
+      fprintf(stderr,"usage: %s <MQTT Broker Addr> <MQTT Broker port> <udp bridge server port>\n", argv[0]);
       exit(0);
    }
+  strBrokerAddr = argv[1];
+
+  brokerPort = atoi(argv[2]);
+  udpPort = atoi(argv[3]);
+
   signal(SIGPIPE, signal_callback_handler);
   sem_init(&receivedUdpMutex, 0, 0);
   sem_init(&receivedTcpMutex, 0, 0);
-  sem_init(&firstConnectMutex,0, 0);
+  sem_init(&tcpMsgWait,0, 0);
   pthread_t udp_pth_id,tcp_pth_id;	// this is our thread identifier
-  int i = 0;
 
   /* Create worker thread */
   pthread_create(&udp_pth_id,NULL,UDPServer,argv);
--- udpclient.c
+++ udpclient.c
@@ -6,12 +6,13 @@
 #include<stdlib.h> //exit(0);
 #include<arpa/inet.h>
 #include<sys/socket.h>
+#include <unistd.h>
 #include "MQTTPacket.h"
 
 #define SERVER "192.168.1.88"
 #define BUFLEN 1024  //Max length of buffer
 #define PORT 8888   //The port on which to send data
-#define PAYLOAD "HELLO"
+#define PAYLOAD "Hello"
 #define TOPIC "a/b"
 
 void die(char *s)
@@ -20,32 +21,52 @@ void die(char *s)
     exit(1);
 }
 
+enum states{
+  NOT_CONNECTED,
+  CONNECTED,
+  PUBLISHED
+};
+ enum states state = NOT_CONNECTED;
+
+void printMessage(unsigned char *buf,int len,const char* description){
+  int i;
+  printf("%s\n",description);
+  for (i = 0; i < len; i++){
+    printf("[0x%02x]",buf[i]);
+  }
+  printf("\n");
+}
+
 int main(void)
 {
     struct sockaddr_in si_other;
-    int s, i, slen=sizeof(si_other),len_connect;
+    int s, slen=sizeof(si_other),len_connect,len_publish;
     char buf[BUFLEN];
     char message[BUFLEN];
     unsigned char MQTTPacket[BUFLEN];
+    unsigned char MQTTPucblish[BUFLEN];
+    int packetType;
+    int packetLen;
     MQTTPacket_connectData *connect_options;
     MQTTPacket_publishData *publish_options;
 
     /*Connect packet serialization example */
     MQTTPacket_connectData default_options_connect = MQTTPacket_connectData_initializer;
-    default_options_connect.MQTTVersion = 4;
+    default_options_connect.MQTTVersion = 3;
     default_options_connect.keepAliveInterval = 10;
     default_options_connect.clientID.cstring = "FRDM-K64F";
     connect_options = &default_options_connect;
     len_connect = MQTTSerialize_connect(MQTTPacket,BUFLEN,connect_options);
-#if 0
+
     /*publish packet serialization example */
     MQTTPacket_publishData default_options_publish = MQTTPacket_publishData_initializer;
     publish_options = &default_options_publish;
     publish_options->topicName.cstring = TOPIC;
-    publish_options->payload = PAYLOAD;
+    publish_options->payload = (unsigned char *)PAYLOAD;
     publish_options->payloadlen = strlen((const char *)PAYLOAD);
-    len_connect = MQTTSerialize_publish_opt(MQTTPacket,BUFLEN,publish_options);
-#endif
+    len_publish = MQTTSerialize_publish_opt(MQTTPucblish,BUFLEN,publish_options);
+
+
     if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
         die("socket");
@@ -64,24 +85,59 @@ int main(void)
     while(1)
     {
         printf("Enter to send packet: ");
-        gets(message);
+        fgets( message, 10, stdin );
 
-        //send the message
-        if (sendto(s, MQTTPacket, len_connect , 0 , (struct sockaddr *) &si_other, slen)==-1)
-        {
-            die("sendto()");
+        //send the connect message
+        if (state == NOT_CONNECTED){
+          printMessage(MQTTPacket,len_connect,"Sending Connect:");
+          if (sendto(s, MQTTPacket, len_connect , 0 , (struct sockaddr *) &si_other, slen)==-1)
+          {
+              die("sendto()");
+          }
+          printMessage(MQTTPucblish,len_publish,"Sending PuConnect:");
+          if (sendto(s, MQTTPucblish, len_publish , 0 , (struct sockaddr *) &si_other, slen)==-1)
+          {
+              die("sendto()");
+          }
         }
 
+        /* Send Publish Messages */
+        if (state == CONNECTED){
+          printMessage(MQTTPucblish,len_publish,"Sending Publish:");
+          if (sendto(s, MQTTPucblish, len_publish , 0 , (struct sockaddr *) &si_other, slen)==-1)
+          {
+              die("sendto()");
+          }
+          state = PUBLISHED;
+        }
         //receive a reply and print it
         //clear the buffer by filling null, it might have previously received data
         memset(buf,'\0', BUFLEN);
         //try to receive some data, this is a blocking call
-        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
-        {
-            die("recvfrom()");
+        if (state != PUBLISHED){
+          if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, (socklen_t *)&slen) == -1)
+          {
+              die("recvfrom()");
+          }
+          packetType = ((int)buf[0]) >> 4;
+          switch (packetType){
+            case CONNACK:
+              packetLen = buf[1];
+              printMessage((unsigned char *)buf,packetLen,"CONNACK;");
+              if ((packetLen == 2) && buf[2] == 0 && buf[3] == 0){
+                state = CONNECTED;
+                }
+              else
+                printf("connection rejected try again\n");
+              break;
+            case PUBACK:
+              printf("Message Correctly Published");
+              packetLen = buf[1];
+            default:
+              break;
+          }
         }
-
-        puts(buf);
+        state = CONNECTED;
     }
 
     close(s);
